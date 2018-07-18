e7d048fa848216587ba15eec40b0ac64dd0e5755
Garry Clerge <clergeg@gmail.com>
2018-07-18 16:19:39 -0400

    testing

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,14 +1,6 @@
 class Adapter {
 
-  static getShows (){
-    return fetch("http://api.tvmaze.com/shows")
-    .then(res => res.json())
-  }
 
-  static getShowEpisodes (showID){
-    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
-    .then(res => res.json())
-  }
 
 }
 
