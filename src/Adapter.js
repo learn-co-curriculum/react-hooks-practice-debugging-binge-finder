d2e1d649b594a7eafb78863cd4dac6d6c3e04fa6
Garry Clerge <clergeg@gmail.com>
2018-07-19 13:26:21 -0400

    merged solution with master branch

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,6 +1,14 @@
 class Adapter {
 
+  static getShows (){
+    return fetch("http://api.tvmaze.com/shows")
+    .then(res => res.json())
+  }
 
+  static getShowEpisodes (showID){
+    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
+    .then(res => res.json())
+  }
 
 }
 
